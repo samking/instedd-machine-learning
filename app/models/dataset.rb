cc21f6ff1058bf348d7ab6a12d4e928254f9e0f2
Sam King <samking@cs.stanford.edu>
2010-07-07 09:59:46 +0700

    changed name of 'remote db config'

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,74 +2,87 @@ require 'csv'
 require 'open-uri'
 
 class Dataset < ActiveRecord::Base
-	#TODO: support other database services
-	@@sdb = Aws::SdbInterface.new($db_config[:sdb_user], $db_config[:sdb_pass])
-	def self.sdb
-		@@sdb
-	end
+  #TODO: support other database services
+  @@sdb = Aws::SdbInterface.new($db_config[:sdb_user], $db_config[:sdb_pass])
+  def self.sdb
+    @@sdb
+  end
 
-	#validation
-	validates_uniqueness_of :uid
-	validates_length_of :uid, :minimum => 3
-	validate_on_create :must_not_duplicate_database_tables 
+  #validation
+  validates_uniqueness_of :uid
+  validates_length_of :uid, :minimum => 3
+  validate_on_create :must_not_duplicate_database_tables 
 
-	#sets up the online database
-	after_create :create_remote_database
-	before_destroy :remove_remote_database
+  #sets up the online database
+  after_create :create_remote_database
+  before_destroy :remove_remote_database
 
-	#uid can never change
-	attr_readonly :uid
+  #uid can never change
+  attr_readonly :uid
 
-	#TODO: support formats other than csv
-	#TODO: support csv without header (specify noheader by parameter)
-	#TODO: support parameters to specify rows, cols, and services
-	def learn_from_data(dataUrl)
-		reader = CSV::Reader.create(open(dataUrl))
-		header = reader.shift
+  #TODO: support formats other than csv
+  #TODO: support csv without header (specify noheader by parameter)
+  #TODO: support parameters to specify rows, cols, and services
+  def learn_from_data(dataUrl)
+    reader = CSV::Reader.create(open(dataUrl))
+    header = reader.shift
 
-		items = []
-		reader.each do |row| 
-			attributes = {}
-			header.zip(row) do |col_head, col|
-				attributes[col_head] = col
-			end
-			items << Aws::SdbInterface::Item.new(UUIDTools::UUID.timestamp_create, attributes)
-		end
-		@@sdb.batch_put_attributes(uid, items)
-	end
+    items = []
+    reader.each do |row| 
+      attributes = {}
+      header.zip(row) do |col_head, col|
+        attributes[col_head] = col
+      end
+      items << Aws::SdbInterface::Item.new(UUIDTools::UUID.timestamp_create, attributes)
+    end
+    @@sdb.batch_put_attributes(uid, items)
+  end
 
-	#TODO: ask machine learning services if we're still learning
-	def is_learning?
-		false
-	end
+  #TODO: ask machine learning services if we're still learning
+  def is_learning?
+    false
+  end
 
-	def get_items
-		items = []
-		@@sdb.select("select * from #{uid}")[:items].each do |item|
-			item.each do |key, val|
-				items << val
-			end
-		end
-		items
-	end
+  def get_items
+    items = []
+    @@sdb.select("select * from #{uid}")[:items].each do |item|
+      item.each do |key, val|
+        items << val
+      end
+    end
+    items
+  end
 
-	def self.get_domain_list_string
-		@@sdb.list_domains[:domains].inspect
-	end
+  def self.get_domain_list_string
+    @@sdb.list_domains[:domains].inspect
+  end
 
-	private
+  def sdb_to_xml
+    items = get_items
+    builder = Builder::XmlMarkup.new
+    builder.instruct!
+    items.each do |item|
+      builder.item {
+        item.each do |key, value|
+          eval "builder.#{key}('#{value}')"
+        end
+      }
+    end
+    builder.target!
+  end
 
-	def create_remote_database
-		@@sdb.create_domain(uid)
-	end
+  private
 
-	def remove_remote_database
-		@@sdb.delete_domain(uid)
-	end
+  def create_remote_database
+    @@sdb.create_domain(uid)
+  end
 
-	def must_not_duplicate_database_tables 
-    	errors.add :uid , 'already in remote database.' if @@sdb.list_domains[:domains].include?(uid)
-	end
+  def remove_remote_database
+    @@sdb.delete_domain(uid)
+  end
 
+  def must_not_duplicate_database_tables 
+    errors.add :uid , 'already in remote database.' if @@sdb.list_domains[:domains].include?(uid)
+  end
 end
 
