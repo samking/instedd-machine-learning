bca9d32c16569de8040462f3b7db81e249679e27
Sam King <samking@cs.stanford.edu>
2010-07-29 16:29:39 +0700

    cosmetic changes

--- features/step_definitions/database_data_add_steps.rb
+++ features/step_definitions/database_data_add_steps.rb
@@ -2,11 +2,11 @@ require File.expand_path(File.join(File.dirname(__FILE__), "quoted_arg_constant"
 
 When /^the test client adds the #{QUOTED_ARG} file to the database using the web interface$/ do |filename|
   visit dataset_path 'test'
-  Then "I fill in \"dataurl\" with \"http://localhost:3000/tests/#{filename}\""
+  Then "I fill in \"data_url\" with \"http://localhost:3000/tests/#{filename}\""
   Then 'I press "Modify Data and/or run Machine Learning"'
 end
 
 When /^the test client adds the #{QUOTED_ARG} file to the database using the api$/ do |filename|
-  visit(dataset_path('test') + '.xml', :put, :dataurl => "/tests/#{filename}")
+  visit(dataset_path('test') + '.xml', :put, :data_url => "/tests/#{filename}")
 end
 
